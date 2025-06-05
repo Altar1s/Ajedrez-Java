6936d45835faba8325dac6f8b83600ed4f0c74d9
Gerhard <167511467+Altar1s@users.noreply.github.com>
2025-06-05 23:44:42 +0200

    mejora carpeta
    si

---
+++ src/Ajedrez/Piezas/Casilla.java
@@ -0,0 +1,13 @@
+package Ajedrez.Piezas;
+
+import Ajedrez.Tablero;
+
+public class Casilla extends Pieza {
+	
+	public Casilla(Tablero tablero, int coorX, int corrY) {
+		super(tablero, coorX, corrY);
+		img = null;
+	}
+	
+	
+}
